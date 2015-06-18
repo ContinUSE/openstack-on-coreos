d021f923c9c257a27b7011316718452652c65fd4
ContinUSE <continuse@icloud.com>
2015-06-18 16:16:57 +0900

    admin_openrc.sh for kilo

---
+++ cluster/continuse/script/admin_openrc.sh
@@ -0,0 +1,8 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=admin
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=$ADMIN_PASS
+export OS_AUTH_URL=http://controller:35357/v3
+export OS_IMAGE_API_VERSION=2
