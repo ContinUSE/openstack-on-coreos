ebc77614f299719af108e265b9be4f31dcd5db3c
ContinUSE <continuse@icloud.com>
2015-06-18 16:17:16 +0900

    demo_openrc.sh for kilo

---
+++ cluster/continuse/script/demo_openrc.sh
@@ -0,0 +1,8 @@
+export OS_PROJECT_DOMAIN_ID=default
+export OS_USER_DOMAIN_ID=default
+export OS_PROJECT_NAME=demo
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=$DEMO_PASS
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IMAGE_API_VERSION=2
